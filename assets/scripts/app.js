d54c54ef39070b8d85cae925a25a538e95939e1d
Bruno Bakarić <bak.13@hotmail.com>
2022-01-17 15:05:14 +0100

    some of scss implemented, changes may occur

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,14 +10,17 @@ class InputNewTodo {
 		const renderHook = document.getElementById('list-input');
 		const inputForm = document.createElement('form');
 		inputForm.innerHTML = `
-            <input type="text" id="list-name" placeholder="Add new list ..."></input>
-            <button id="submitBtn">Add</button
+            <input type="text" id="list-name" placeholder="Add new list ..." autofocus="true"></input>
+            <button>+</button>
         `;
 		renderHook.append(inputForm);
 		const addNewList = document.querySelector('button');
 		addNewList.addEventListener('click', (e) => {
 			e.preventDefault();
 			const inputValue = document.getElementById('list-name');
+			if (inputValue.value.trim() === '') {
+				return;
+			}
 			const todoList = new TodoList();
 			todoList.title = inputValue.value;
 			console.log(`Add new todo clicked!`, todoList);
@@ -37,17 +40,26 @@ class NewNotes {
 		const noteEl = document.createElement('li');
 		noteEl.innerHTML = `
 			<p>${this.note}</p>
-			<button id="delete">X</button>
-			<button id="completed">+</button>
+			<div>
+				<button id="delete">
+					<i class="fas fa-minus-circle fa-lg"></i>
+				</button>
+				<button id="completed">
+					<i class="fas fa-check-circle fa-lg"></i>
+				</button>
+			</div>
 		`;
 		list.append(noteEl);
 		const deleteNoteBtn = noteEl.querySelector('button');
-		const completedBtn = document.getElementById('completed');
+		const completedBtn = noteEl.querySelector('button:nth-of-type(2)');
 		deleteNoteBtn.addEventListener('click', () => {
-			noteEl.remove();
+			noteEl.classList.add('delete-note');
+			noteEl.addEventListener('transitionend', () => {
+				noteEl.remove();
+			});
 		});
 		completedBtn.addEventListener('click', () => {
-			noteEl.querySelector('p').style.textDecoration = 'line-through';
+			noteEl.classList.add('completed');
 		});
 
 		console.log(`New note: ${this.note}`);
@@ -62,17 +74,22 @@ class TodoListCard {
 	render() {
 		const renderHook = document.getElementById('list-container');
 		const todoEl = document.createElement('div');
+		todoEl.classList.add('todo-list-card');
 		todoEl.innerHTML = `
-            <h3>${this.todoInfo.title}</h3>
-			<button id="delete">X</button>
+			<div id="card-header">
+            	<h3>${this.todoInfo.title}</h3>
+				<button>
+					<i class="far fa-trash-alt fa-lg"></i>
+				</button>
+			</div>
 			<form>
             	<input type="text" placeholder="Add new note ..."></input>
-            	<button>Add</button>
+            	<button>+</button>
 			</form>
             <ul></ul>
         `;
 		renderHook.append(todoEl);
-		const deleteListBtn = document.getElementById('delete');
+		const deleteListBtn = todoEl.querySelector('button');
 		const form = todoEl.querySelector('form');
 		const submitBtn = form.querySelector('button');
 		const notesList = todoEl.querySelector('ul');
@@ -82,6 +99,9 @@ class TodoListCard {
 		submitBtn.addEventListener('click', (e) => {
 			e.preventDefault();
 			const inputNotes = todoEl.querySelector('input').value;
+			if (inputNotes.trim() === '') {
+				return;
+			}
 			const todoList = new TodoList(this.todoInfo.title, inputNotes);
 			const displayNotes = new NewNotes(todoList.notes);
 			console.log(`Add new notes clicked!`, todoList);
